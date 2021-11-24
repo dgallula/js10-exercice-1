d80c320850d7e207d4fd3a5783170d91c5182e63
galluladavid <82768589+dgallula@users.noreply.github.com>
2021-11-24 10:21:28 +0000

    Initial commit

---
+++ script.js
@@ -0,0 +1,33 @@
+// exercice  
+const button1 = document.querySelector("button-id")
+const button2 = document.querySelector("button-stop-id")
+
+
+ function firstAction ()  {
+   console.log("start")
+   
+ }
+
+ function secondAction () {
+    console.log("end")
+ }
+
+
+
+ function clock() {
+   let d = new Date();
+  console.log(d.getHours() + ":" + d.getMinutes() + ":" +d.getSeconds())
+ }
+
+
+  firstAction()
+  secondAction() 
+  clock()
+
+
+
+   
+   
+    
+
+
