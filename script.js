6589fc6fa154133e6fd85faba974d4d05c7875da
gallula david <82768589+dgallula@users.noreply.github.com>
2021-11-25 07:07:34 +0000

    exercices 1,2,3,4

--- script.js
+++ script.js
@@ -1,4 +1,32 @@
-// exercice  
+// exercice  1
+const button1 = document.querySelector("button-id")
+const button2 = document.querySelector("button-stop-id")
+
+
+ function firstAction ()  {
+   console.log("start")
+   
+ }
+
+ function secondAction () {
+    console.log("end")
+ }
+
+
+
+ function clock() {
+   let d = new Date();
+  console.log(d.getHours() + ":" + d.getMinutes() + ":" +d.getSeconds())
+ }
+
+
+  firstAction()
+  secondAction() 
+  clock()
+
+ 
+
+ // exercice  2 
 const button1 = document.querySelector("button-id")
 const button2 = document.querySelector("button-stop-id")
 
@@ -9,9 +37,12 @@ const button2 = document.querySelector("button-stop-id")
  }
 
    function clock() {
+     setTimeout(()=> {
    let d = new Date();
   console.log(d.getHours() + ":" + d.getMinutes() + ":" +d.getSeconds())
- }
+ },3000);
+ 
+    }
    
 
  function secondAction () {
@@ -24,7 +55,83 @@ const button2 = document.querySelector("button-stop-id")
   clock()
   secondAction ()
 
+  //  exercice 3 
 
+const button1 = document.querySelector("button-id")
+const button2 = document.querySelector("button-stop-id")
+
+
+  function consoleLog() {
+   console.log("start")
+   let currentime = Date();
+   setTimeout(()=> {
+     console.log(currentime)
+   },3000);
+   console.log ("end ")
+  }
+   consoleLog()
+ 
+
+setInterval(myFunction2, 1000);
+
+ function myFunction2() {
+  let d = new Date();
+  document.getElementById("clock").innerHTML=
+  d.getHours() + ":" +
+  d.getMinutes() + ":" +
+   d.getSeconds();
+}
+
+exercice 4
+
+const button1 = document.querySelector("button-id")
+const button2 = document.querySelector("button-stop-id")
+
+function display() {
+   document.getElementById("start").innerHTML= "start"
+   document.getElementById("end").innerHTML= "end"
+}
+
+function random1 () {
+
+let random1 = Math.floor(Math.random()*101);
+setTimeout(()=> {
+document.getElementById("random1").innerHTML= random1 
+},3000);
+
+}
+
+
+function random2 () {
+
+let random2 = Math.floor(Math.random()*101);
+setTimeout(()=> {
+document.getElementById("random2").innerHTML= random2
+},5000);
+
+}
+
+
+function random3 (){
+
+let random3 = Math.floor(Math.random()*101);
+setTimeout(()=>{
+document.getElementById("random3").innerHTML= random3
+},7000);
+
+}
+
+display();
+random1();
+random2();
+random3();
+
+
+
+ 
+
+   
+   
     
 
 
